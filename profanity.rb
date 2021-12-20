87d7b666dbefef895e712ace6de5fd4f3af0b0d7
Mahtra <mahtra@seul.org>
2021-12-21 12:41:50 +1300

    Fix for my previous "fix"

--- profanity.rb
+++ profanity.rb
@@ -864,9 +864,6 @@ key_action = Hash.new
 need_prompt = false
 prompt_text = ">"
 stream_handler = Hash.new
-ExpWindow = nil
-moonWindow = nil
-percWindow = nil
 indicator_handler = Hash.new
 progress_handler = Hash.new
 countdown_handler = Hash.new
@@ -1426,7 +1423,7 @@ load_layout = proc { |layout_id|
 						stream_handler['exp'] = ExpWindow.new(height, width - 1, top, left)
 						stream_handler['exp'].logger = logger
 					elsif e.attributes['class'] == 'moonWindow'
-						stream_handler['moonWindow'] = PercWindow.new(height, width - 1, top, left)
+						stream_handler['moonWindow'] = moonWindow.new(height, width - 1, top, left)
 						stream_handler['moonWindow'].logger = logger
 					elsif e.attributes['class'] == 'percWindow'
 						stream_handler['percWindow'] = PercWindow.new(height, width - 1, top, left)
